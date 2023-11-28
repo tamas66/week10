2ec768c271e077ff6174b286474493a113ff9fd6
Tamas Tacsi <tamas512@gmail.com>
2023-11-28 08:50:34 +0100

    final

--- JokesApplicationSolution/wwwroot/scripts/geek.js
+++ JokesApplicationSolution/wwwroot/scripts/geek.js
@@ -1,1 +1,26 @@
 ﻿var viccek;
+
+var letoltes = function () {
+    fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+}
+
+window.onload = letoltes
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    for (let i = 0; i < viccek.length; i++) {
+        // Új DOM elem létrehozása
+        let viccElem = document.createElement('div');
+
+        // A vicc tartalmának beállítása az elemen belül
+        viccElem.textContent = viccek[i];
+
+        // A DOM elem hozzáadása a dokumentumhoz
+        document.body.appendChild(viccElem);
+    }
+}
