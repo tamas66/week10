41d59c0278fd8c18a2e184f32fbe9c50e1cbb05f
Tamas Tacsi <tamas512@gmail.com>
2023-12-04 23:46:08 +0100

    second step

---
+++ week11/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using JokesApplicationSolution.hajosModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace week11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
